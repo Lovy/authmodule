426007cc798c85111fee1efe89b09c2f8171db67
lovy <loveygupta007@gmail.com>
2014-12-29 20:44:51 +0530

    formatted the index file

--- index.js
+++ index.js
@@ -1,12 +1,72 @@
 var expressJwt = require('express-jwt');
 var jwt = require('jsonwebtoken');
-var express = require('express');
-var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient
+, assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+
+//Secret
+var secret = "yoursecrettoken";
+//Connection URL
+var url = 'mongodb://localhost:27017/mydb';
+//Collection
+var mycollection ='users'
+
+var connect = function(callback){
+	MongoClient.connect(url, function(err, db) {
+		  assert.equal(null, err);
+		  console.log("Connected correctly to server");
+		  callback(db);
+	});
+};
+
+var insertUser = function(db, data) {
+	  // Get the documents collection
+	  var collection = db.collection(mycollection);
+	  // Insert user data
+	  collection.insert(data, function(err, result) {
+	    assert.equal(err, null);
+	    console.log(result[0]._id);
+	    db.close();
+	  });
+};
+
+var register_user = function(req,res){
+	var data = JSON.parse(req.body.data);
+	var mongoconnection = connect(function(db){
+		insertUser(db,data);
+	});
+	// We are sending the profile inside the token
+	var token = jwt.sign(data,secret, { expiresInMinutes: 60*5 });
+	res.json({ token: token });
+};
+
+
+
+var findUser = function(db, id, callback) {
+	  // Get the documents collection
+	  var collection = db.collection('users');
+	  var _id = new ObjectId(id);
+	  collection.find({_id:_id}).toArray(function(err, result) {
+	    assert.equal(err, null);
+	    db.close();
+	    callback(result);
+	  });
+};
+
+var getUserDetails = function(req,res){
+	var id = req.params.userid;
+	var mongoconnection = connect(function(db){
+		findUser(db,id,function(result){
+			console.log(result);
+			res.end();
+		});
+	});
+}
 
 var authenticate = function (req, res) {
 	  //TODO validate req.body.username and req.body.password
 	  //if is invalid, return 401
-	  if (!(req.body.username === 'john.doe' && req.body.password === 'foobar')) {
+	  if (!(req.body.username === 'john' && req.body.password === 'foo')) {
 	    res.status(401).send('Wrong user or password');
 	    return;
 	  }
@@ -19,19 +79,17 @@ var authenticate = function (req, res) {
 	  };
 
 	  // We are sending the profile inside the token
-	  var token = jwt.sign(profile, "thisisasecrettokenformentozafuckers", { expiresInMinutes: 60*5 });
+	  var token = jwt.sign(profile, secret, { expiresInMinutes: 60*5 });
 
 	  res.json({ token: token });
 	};
 	
-var secret = "thisisasecrettokenformentozafuckers";
+
 var verify = expressJwt({secret: secret})
 
 var refresh_token = function (req, res) {
   // verify the existing token
-	var secret = "thisisasecrettokenformentozafuckers";
-	console.log(req.headers);
-	var profile;
+  var profile;
   jwt.verify(req.body.token, secret , null , function(err,res){
 	  profile=res;
   });
@@ -52,5 +110,7 @@ var refresh_token = function (req, res) {
 module.exports = {
 	authenticate:authenticate,
 	verify:verify,
-	refresh_token:refresh_token
+	refresh_token:refresh_token,
+	register_user:register_user,
+	getUserDetails:getUserDetails
 };
